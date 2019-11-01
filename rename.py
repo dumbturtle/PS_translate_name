2ef666600c133c32b222bfd157e5edbaf009df22
Aleksandr Semenov <semenlinux@gmail.com>
2019-11-01 07:12:50 +0000

    Считывание файлов

---
+++ rename.py
@@ -0,0 +1,14 @@
+import csv
+import os
+import os.path
+
+'''
+with open('PS54_CSV.csv','r', encoding='cp1251') as file_obj:
+    #file_obj.readline()
+    #lines = csv.reader(file_obj)
+    lines = csv.DictReader(file_obj, delimiter=';')
+    for line in lines:
+       print(line["Обозначение"])
+       print(line["Название ПС"])
+'''
+
